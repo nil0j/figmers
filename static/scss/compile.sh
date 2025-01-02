0c084f2d1ec2d6d48a40cca556050498658f09b3
niliara-edu <nil.jimeno@estudiant.fjaverianas.com>
2025-01-02 12:59:28 +0100

    separate css and nav

--- static/scss/compile.sh
+++ static/scss/compile.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-sassc static/scss/index.scss > static/css/style.css
+sassc static/scss/style.scss > static/css/style.css
+sassc static/scss/header-main.scss > static/css/header-main.css
+sassc static/scss/footer.scss > static/css/footer.css
+sassc static/scss/game-list.scss > static/css/game-list.css
